3bdaa5138aa83e0ed98bf264764f40ae625850e1
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-09 16:24:58 -0300

    feat: implementar endpoint para obter uma Serie por id

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,9 +1,11 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,10 @@ public class SerieController {
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos() {
         return service.obterLacamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return service.obterPorId(id);
     }
 }
