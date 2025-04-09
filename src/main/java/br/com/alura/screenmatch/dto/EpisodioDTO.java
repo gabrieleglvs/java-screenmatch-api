354d6d576423e4d488471cfbb06d53727f34288e
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2025-04-09 17:18:52 -0300

    feat: implementar DTO e endpoint para obter todos os episodios de uma Serie

---
+++ src/main/java/br/com/alura/screenmatch/dto/EpisodioDTO.java
@@ -0,0 +1,8 @@
+package br.com.alura.screenmatch.dto;
+
+import java.time.LocalDate;
+
+public record EpisodioDTO( Integer temporada,
+                           String titulo,
+                           Integer numeroEpisodio) {
+}
