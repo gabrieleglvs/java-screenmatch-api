1e95f6b12e04c712a07a8b3c0560bd864acad1f2
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2026-01-09 15:40:30 -0300

    fix: adicionar opcao AVENTURA em ENUM CATEGORIA

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -5,7 +5,8 @@ public enum Categoria {
     ROMANCE("Romance", "Romance"),
     COMEDIA("Comedy", "Comédia"),
     DRAMA("Drama", "Drama"),
-    CRIME("Crime", "Crime");
+    CRIME("Crime", "Crime"),
+    AVENTURA("Aventura", "Aventura");
 
     private String categoriaOmdb;
     private String categoriaPortugues;
